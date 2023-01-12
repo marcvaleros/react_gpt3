0a631f6a115c51e2b99599845ef56a7478e17cc9
Marc Valeros <marcnathanielvaleros@gmail.com>
2023-01-12 23:09:18 +0800

    'navbar'

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react'
+import './App.css'
 import { Blog,Features,Footer,Header,Possibility,WhatGPT3 } from './containers';
 import {  Brand, CTA, Navbar  } from './components';
 
 const App = () => {
   return (
-    <div>
-      <h1>GPT_3</h1>
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar/>
+        <Header/>
+      </div>
+    <Brand/>
+    <WhatGPT3/>
+    <Features/>
+    <Possibility/>
+    <CTA/>
+    <Blog/>
+    <Footer/>
     </div>
   )
 }
